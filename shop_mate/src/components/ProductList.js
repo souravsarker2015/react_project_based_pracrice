2c5f0b07c887e509432083f11ff575dfb7f7d164
souravsarker2015 <souravsarker2015@gmail.com>
2023-03-16 00:13:35 +0600

    json server and custom hooks

---
+++ shop_mate/src/components/ProductList.js
@@ -0,0 +1,55 @@
+import {useEffect, useState} from "react";
+
+export const ProductList = () => {
+    const [products, setProducts] = useState([])
+    const [url, setUrl] = useState("http://localhost:8000/product")
+    const [counter, setCounter] = useState(0)
+
+    // fetch("http://localhost:8000/product")
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+
+    // console.log(products)
+    useEffect(() => {
+
+        const fetchProducts = async () => {
+            const response = await fetch(url)
+            const data = await response.json()
+            setProducts(data)
+        }
+        fetchProducts();
+
+        // fetch(url)
+        //     .then(response => response.json())
+        //     .then(data => setProducts(data))
+
+
+    }, [url]);
+
+    useEffect(() => {
+        console.log(counter)
+    }, [counter]);
+
+    return (
+        <section>
+            <div className="filter">
+                <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+                <button onClick={() => setUrl("http://localhost:8000/product")}>All</button>
+                <button onClick={() => setUrl("http://localhost:8000/product?in_stock=true")}>In stock</button>
+            </div>
+
+
+            {products.map((product) => (
+                <div className="card" key={product.id}>
+                    <p className={'id'}>{product.id}</p>
+                    <p className={'name'}>{product.name}</p>
+                    <p className={'info'}>
+                        <span>$ {product.price}</span>
+                        <span className={product.in_stock ? "instock" : "unavailable"}> {product.in_stock ? "in stock" : "unavailable"} </span>
+                    </p>
+                </div>
+
+            ))}
+        </section>
+    );
+};
